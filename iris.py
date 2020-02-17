8e431190c2a2c9444802fb966aceac9deee95387
Harsh Dave <harshdave1995@gmail.com>
2020-02-17 18:52:13 -0500

    imported matplot package imported scikit learn package plotted scatter plot for data visualization

--- iris.py
+++ iris.py
@@ -1,2 +1,17 @@
-print("hello")
-print("fromIDE")
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+from pandas.plotting import scatter_matrix
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris_dataset = load_iris()
+
+X_train, X_test, Y_train, Y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+
+grr = scatter_matrix(iris_dataframe, c=Y_train, figsize=(15, 15), marker='o',
+                     hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+plt.show()
